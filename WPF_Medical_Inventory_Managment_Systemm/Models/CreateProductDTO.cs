aebcf087e1ac3a55411185e2a480082070e7e5c1
Prajyot5015 <prajyotpathade5015@gmail.com>
2025-04-16 14:40:19 +0530

    ModifiedFiles

---
+++ WPF_Medical_Inventory_Managment_Systemm/Models/CreateProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Medical_Inventory_Managment_Systemm.Models
+{
+    public class CreateProductDTO
+    {
+        public string Name { get; set; }
+        public string Batch { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string Unit { get; set; }
+        public string Price { get; set; }
+        public int BrandId { get; set; }
+        public int ManufacturerId { get; set; }
+    }
+}
