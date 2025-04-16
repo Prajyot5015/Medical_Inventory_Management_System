f020a37da62443b85b86996f1e7a1960c4342ab7
Prajyot5015 <prajyotpathade5015@gmail.com>
2025-04-16 19:54:07 +0530

    Implemented Update And Delete Endpoint

--- WPF_Medical_Inventory_Managment_Systemm/Helpers/RelayCommands/RelayCommand.cs
+++ WPF_Medical_Inventory_Managment_Systemm/Helpers/RelayCommands/RelayCommand.cs
@@ -1,25 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace WPF_Medical_Inventory_Managment_Systemm.Helpers.RelayCommands
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        private readonly Func<Task> _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly Func<T, Task> _execute;
+        private readonly Predicate<T> _canExecute;
 
-        public RelayCommand(Func<Task> execute, Func<bool> canExecute = null)
+        public RelayCommand(Func<T, Task> execute, Predicate<T> canExecute = null)
         {
             _execute = execute;
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
-        public async void Execute(object parameter) => await _execute();
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            await _execute((T)parameter);
+        }
+
         public event EventHandler CanExecuteChanged;
     }
 }
