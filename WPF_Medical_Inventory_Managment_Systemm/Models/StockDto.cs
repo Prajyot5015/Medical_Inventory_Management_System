0ae2289abd13dbfa47d926ec5f9ee2deaf16e0ce
ramkishanC <ramkishanchame@gmail.com>
2025-04-21 10:39:52 +0530

    "stock updated"

---
+++ WPF_Medical_Inventory_Managment_Systemm/Models/StockDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Medical_Inventory_Managment_Systemm.Models
+{
+    public class StockDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Batch { get; set; }
+        public int CurrentStock { get; set; }
+        public int LowStockThreshold { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+
+
+        public string BrandName { get; set; }
+        public string ManufacturerName { get; set; }
+
+    }
+
+}
---
+++ WPF_Medical_Inventory_Managment_Systemm/Views/StockView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Medical_Inventory_Managment_Systemm.ViewModels;
+
+namespace WPF_Medical_Inventory_Managment_Systemm.Views
+{
+    /// <summary>
+    /// Interaction logic for StockView.xaml
+    /// </summary>
+    public partial class StockView : Page
+    {
+        private readonly StockViewModel _viewModel;
+
+        public StockView()
+        {
+            InitializeComponent();
+            _viewModel = new StockViewModel();
+            DataContext = _viewModel;
+
+            Loaded += async (s, e) =>
+            {
+                await _viewModel.LoadStockData();
+                await _viewModel.LoadLowStockData();
+                await _viewModel.LoadNearExpiryData();
+            };
+        }
+
+        private void LowStockDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void NearExpiryDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
